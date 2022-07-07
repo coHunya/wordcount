9d85d032f4b0b1c2b76031c09c82b15363dfa13c
coHunya <andrey1798@gmail.com>
2022-07-07 20:30:32 +0300

    Implement the logic

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	str := readInput()
+	result := returnCount(str)
+	fmt.Println(result)
+}
+
+func readInput() string {
+	var str string
+	if len(os.Args) > 1 {
+		str = os.Args[1]
+	}
+	return str
+}
+
+func returnCount(str string) int {
+	counter := 0
+	if len(str) == 0 {
+		return counter
+	}
+	words := strings.Split(str, " ")
+	for _, w := range words {
+		if len(w) != 0 {
+			counter += 1
+		}
+	}
+	return counter
 }
